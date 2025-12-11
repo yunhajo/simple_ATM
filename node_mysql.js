26eff254970d47c3127b61dbef3b4326d58dda64
yunhajo03 <yjo@caltech.edu>
2025-12-11 17:19:21 +0900

    Updated querites for node_mysql.js file

--- node_mysql.js
+++ node_mysql.js
@@ -3,11 +3,18 @@ const express = require("express");
 const app = express();
 const multer = require("multer");
 const mysql = require("promise-mysql");
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const saltRounds = 12;
+const pinHash = await bcrypt.hash(pin, saltRounds);
 
 // To handle different POST formats
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(multer().none());
+
+app.use(cookieParser());
+const COOKIE_EXP_TIME = 5 * 60 * 1000; // login cookies last 5 minutes.
 
 const DEBUG = true;
 const SERVER_ERROR = "Something went wrong on the server... Please try again later.";
@@ -17,6 +24,171 @@ const SERVER_ERR_CODE = 500;
 app.use(express.static("public"));
 
 /*---- SELECT queries ------ */
+
+/**
+ * Gets account balance
+ */
+app.get("/balance", async (req, res) => {
+    let db;
+    try {
+      db = await getDB();
+      let cardnumber = req.cookies.cardnumber;
+      let balance = await getAccountBalance(db, cardnumber);
+      res.json(balance);
+    } catch (error) {
+      res.type("text");
+      res.status(SERVER_ERR_CODE).send(SERVER_ERROR);
+    }
+    if (db) {
+      db.end();
+    }
+  });
+
+/**
+  * Calls query to get the current account balance
+ */
+
+async function getAccountBalance(db, cardnumber) {
+  let query = 'SELECT balance FROM accounts WHERE card_number= ?'
+  let rows = await db.query(query, [cardnumber]);
+  return rows;
+}
+
+/*---- INSERT queries ------ */
+
+/**
+ * Deposits into account
+ */
+app.post("/deposit", async (req, res) => {
+    let db;
+    try {
+      db = await getDB();
+      let amount = req.body.amount;
+      let cardnumber = req.cookies.cardnumber;
+  
+      let newBalance = await deposit(db, cardnumber, amount);
+      res.json({ balance: newBalance });
+    } catch (error) {
+      res.type("text");
+      res.status(SERVER_ERR_CODE).send(SERVER_ERROR);
+    }
+    if (db) {
+      db.end();
+    }
+  });
+
+/**
+ * Helper function for deposit
+ */
+
+async function deposit(db, cardnumber, amount) {
+
+    let [result] = await db.query(
+        "SELECT balance FROM accounts WHERE card_number = ?",
+        [cardnumber]
+    );
+    let before = result.balance;
+    let after = before + amount;
+
+    await db.query(
+        "UPDATE accounts SET balance = ? WHERE card_number = ?",
+        [after, cardnumber]
+    );
+
+    await db.query(
+        "INSERT INTO transactions (card_number, date, balance_before, balance_after) VALUES (?, NOW(), ?, ?)",
+        [cardnumber, before, after]
+    );
+
+    return after;
+}
+
+/**
+ * Withdraws from account balance
+ */
+app.post("/withdraw", async (req, res) => {
+    let db;
+    try {
+      db = await getDB();
+      let amount = req.body.amount;
+      let cardnumber = req.cookies.cardnumber;
+  
+      let newBalance = await withdraw(db, cardnumber, amount);
+      res.json({ balance: newBalance });
+    } catch (error) {
+      res.type("text");
+      res.status(SERVER_ERR_CODE).send(SERVER_ERROR);
+    }
+    if (db) {
+      db.end();
+    }
+  });
+
+/**
+ * Helper function for withdraw
+ */
+async function withdraw(db, cardnumber, amount) {
+
+    let [result] = await db.query(
+        "SELECT balance FROM accounts WHERE card_number = ?",
+        [cardnumber]
+    );
+    let before = result.balance;
+
+    if (before < amount) {
+        throw new Error("Insufficient funds");
+    }
+    let after = before - amount;
+
+    await db.query(
+        "UPDATE accounts SET balance = ? WHERE card_number = ?",
+        [after, cardnumber]
+    );
+
+    await db.query(
+        "INSERT INTO transactions (card_number, date, balance_before, balance_after) VALUES (?, NOW(), ?, ?)",
+        [cardnumber, before, after]
+    );
+
+    return after;
+}
+/**
+ * "Login" function that allows users to access 
+ * their account information
+ */
+app.post("/login", async (req, res) => {
+    let db;
+    try {
+      db = await getDB();
+      const { cardnumber, pin } = req.body;
+
+      let [rows] = await db.query(
+        "SELECT pin_hash FROM accounts WHERE card_number = ?",
+        [cardnumber]
+      );
+
+      if (!rows) {
+        return res.status(401).json({ error: "Invalid card number" });
+      }
+
+      let valid = bcrypt.compare(pin, rows.pin_has);
+      
+      if (valid) {
+        res.cookie("cardnumber", cardnumber, { maxAge: COOKIE_EXP_TIME });
+        res.type("text");
+        res.send(`Success`);
+      }
+      else {
+        res.status(401).send("Invalid login credentials.");
+      }
+    } catch (error) {
+      res.type("text");
+      res.status(SERVER_ERR_CODE).send(SERVER_ERROR);
+    }
+    if (db) {
+      db.end();
+    }
+  });
 
 /**
   * Error Handler function
